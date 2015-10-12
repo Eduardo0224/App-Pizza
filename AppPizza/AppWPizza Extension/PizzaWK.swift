8caa742c6aec3f90a79f351ae6d50d0473bed2c2
Eduardo <kronnen0224@gmail.com>
2015-10-12 10:37:02 -0500

    Añadí esquema Apple Watch, agregue vista de inicio, configure los elementos de UI de la primera vista: selección de tamaño e implemente el código necesario para popular el WKPicker y obtener el valor seleccionado.

---
+++ AppPizza/AppWPizza Extension/PizzaWK.swift
@@ -0,0 +1,23 @@
+//
+//  PizzaWK.swift
+//  AppPizza
+//
+//  Created by Eduardo on 12/10/15.
+//  Copyright © 2015 EduardoAndrade. All rights reserved.
+//
+
+import Foundation
+
+
+class PizzaWK {
+    var tamano: String?
+    var tipoMasa: String?
+    var tipoQueso: String?
+    var ingredientes: [String]?
+    
+    init ()
+    {
+        
+    }
+
+}
