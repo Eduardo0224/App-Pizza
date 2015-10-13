654c7855ca55d6de496c9a10aa48ed02c6ffbfc7
Eduardo <kronnen0224@gmail.com>
2015-10-13 06:55:21 -0500

    Añadí archivos del proyecto que no fueron subidos en el comió anterior

---
+++ AppPizza/AppWPizza Extension/InterfaceController.swift
@@ -0,0 +1,48 @@
+//
+//  InterfaceController.swift
+//  AppWPizza Extension
+//
+//  Created by Eduardo on 12/10/15.
+//  Copyright © 2015 EduardoAndrade. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet var pickerTamañoPizza: WKInterfacePicker!
+    var tamañosPizza = ["Chica", "Mediana", "Grande"]
+    var pizza = PizzaWK()
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        let indiceInicial = 0
+        var pickerItems: [WKPickerItem] = []
+        for i in indiceInicial...(tamañosPizza.count - 1) {
+            let item = WKPickerItem()
+            item.title = "\(tamañosPizza[i])"
+            pickerItems.append(item)
+        }
+        self.pickerTamañoPizza.setItems(pickerItems)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    @IBAction func pickerChanges(value: Int) {
+        pizza.tamano = tamañosPizza[value]
+        print("Tamaño pizza: \(pizza.tamano)")
+    }
+    
+
+}
