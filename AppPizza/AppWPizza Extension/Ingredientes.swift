08bfad39c5a67a4af8b2ae2498b8814fcd337e96
Eduardo <kronnen0224@gmail.com>
2015-10-14 18:53:06 -0500

    Terminada la parte de verificación de ingredientes seleccionados, si son 0, muestra una alerta con el mensaje: "sin ingredientes" y si el usuario quiere seleccionar más de 5, no puede y se muestra una alerta con el mensaje : "Máximo 5 ingredientes"

--- AppPizza/AppWPizza Extension/Ingredientes.swift
+++ AppPizza/AppWPizza Extension/Ingredientes.swift
@@ -17,6 +17,8 @@ class Ingredientes: WKInterfaceController {
     
     var ingredientes = ["Jamón", "Pepperoni", "Pavo", "Salchicha", "Aceituna", "Cebolla", "Pimiento", "Piña", "Anchoa", "Jalapeño", "Maíz Tierno"]
     var booleans : [Bool] = []
+    
+    var ingredientesSeleccionados = [String]()
 
 
     override func awakeWithContext(context: AnyObject?) {
@@ -40,17 +42,24 @@ class Ingredientes: WKInterfaceController {
         }
     }
     
-    func alerta () {
+    @IBAction func confirmarSeleccion() {
+        if ingredientesSeleccionados.count == 0 {
+            alerta("Sin ingredientes", descripccion: "debes agregar al menos un ingrediente")
+        }
+        
+    }
+    
+    func alerta (titulo : String, descripccion: String) {
         let alerta = WKAlertAction(title: "Ok", style: .Default) { () -> Void in
         }
         
-        self.presentAlertControllerWithTitle("Demasiados ingredientes", message: "5 ingredientes maximo", preferredStyle: .Alert, actions: [alerta])
+        self.presentAlertControllerWithTitle(titulo, message: descripccion, preferredStyle: .Alert, actions: [alerta])
     }
     
     override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
         print("Indice: \(rowIndex)")
         
-        alerta()
+        
     
         booleans[rowIndex] = !booleans[rowIndex]
         
@@ -58,13 +67,44 @@ class Ingredientes: WKInterfaceController {
         
 
         if booleans[rowIndex] {
-            row.checkedImage.setImageNamed("check_on_icon.png")
+            
+            if ingredientesSeleccionados.count < 5 {
+                row.checkedImage.setImageNamed("check_on_icon.png")
+                
+                if !ingredientesSeleccionados.contains(ingredientes[rowIndex]) {
+                    ingredientesSeleccionados.append(ingredientes[rowIndex])
+                    
+                }
+                else {
+                    print("\(ingredientes[rowIndex]) ya esta agregado")
+                }
+            }
+            else {
+                alerta("Demasiados ingredientes", descripccion: "máximo 5")
+            }
+            
+            
+
         }
         else {
+            // remover
+            if ingredientesSeleccionados.count > 0 {
+                
+                for ingredienteAEliminar in ingredientesSeleccionados {
+                    if ingredienteAEliminar == ingredientes[rowIndex] {
+                        
+                        // TODO: Remover el elemento por el valor
+                        if let indiceIngredienteAEliminar = ingredientesSeleccionados.indexOf(ingredientes[rowIndex]) {
+                            ingredientesSeleccionados.removeAtIndex(indiceIngredienteAEliminar)
+                        }
+                    }
+                }
+            }
             row.checkedImage.setImageNamed("check_off_icon.png")
         }
         
         print("Booleans \(booleans[rowIndex])")
+        print(ingredientesSeleccionados)
         
     }
 }
